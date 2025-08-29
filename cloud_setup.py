28ba4264361fb5b8de5999289998c72de7411d0d
ridhomul <ridhoanugrah477@gmail.com>
2025-08-29 23:50:44 +0700

    motiondet-v1

---
+++ cloud_setup.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+MQTT_BROKER =  os.environ['MQTT_USERNAME']
+MQTT_PORT = 8883  # TLS/SSL port
+MQTT_USERNAME = os.environ['MQTT_USERNAME'] 
+MQTT_PASSWORD = os.environ['MQTT_PASSWORD']  
+SYSTEM_NAME = os.environ['SYSTEM_NAME']
+CLIENT_ID = f'python-mqtt-{random.randint(0, 1000)}'
+
+# MQTT Topics (same as before)
+STATUS_TOPIC = f"iot/{SYSTEM_NAME}/status"
+SENSOR_TOPIC = f"iot/{SYSTEM_NAME}/sensor"
+ACTION_TOPIC = f"iot/{SYSTEM_NAME}/action"
+SETTINGS_UPDATE_TOPIC = f"iot/{SYSTEM_NAME}/settings/update"
+
+
